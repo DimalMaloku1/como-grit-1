0082ba0d00ab252a05dacfa951398c8173724bda
Dimal Maloku <96485417+DimalMaloku1@users.noreply.github.com>
2026-03-06 22:38:18 +0100

    commit

---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+   <div className="min-h-screen bg-amber-600 ">
+      <h1 className="text-black">Hello Contact Us</h1>
+    </div>
+  )
+}
+
+export default ContactUs
---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+  <div className="min-h-screen bg-amber-600 ">
+      <h1 className="text-black">Hello gallery </h1>
+    </div>
+  )
+}
+
+export default Gallery
